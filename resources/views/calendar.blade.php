b5596684285edbbf5cdfa1c55d3c962230d8bd58
dich1 <daich128tech@gmail.com>
2022-03-30 17:52:53 +0900

    今月から始まる12ヶ月分

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,6 +1,11 @@
 <html>
 <head>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @if(app('env')=='local')
+      <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @endif
+    @if(app('env')=='production')
+      <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
+    @endif
 </head>
 <body>
   <h3 class="title">●●●●（顧客名）様　施工予定表 </h3>
