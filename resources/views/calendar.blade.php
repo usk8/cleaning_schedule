90f0426a51301518d00b5ad871a230587d5e8337
dich1 <daich128tech@gmail.com>
2022-03-30 16:29:33 +0900

    今月から始まる12ヶ月分

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-  <h3>●●●●（顧客名）様　施工予定表 </h3>
+  <h3 class="title">●●●●（顧客名）様　施工予定表 </h3>
     <div class="items">
       <div id='calendar1' class="item" ></div>
       <div id='calendar2' class="item" ></div>
@@ -18,14 +18,21 @@
       <div id='calendar11' class="item" ></div>
       <div id='calendar12' class="item" ></div>
     </div>
-    <p>お客様の商品交換日は○印の日です。</p>
-    <p>◆レンタルモップ　◆レンタルマット　◆レンタル浄水器</p>
-    <p>★レンジフードフィルター　★エアコンクリーニング　★ハウスクリーニング</p>
-    <img class="fit-picture" src="icon.png" alt="" height="250" width="250">
-    <p>株式会社クライズ</p>
-    <p>埼玉県北本市中丸7-153</p>
-    <p>TEL　０４８ー５０１ー７６５３</p>
-    <p>ﾌﾘｰﾀﾞｲｱﾙ　０１２０ー２１２１ー７４</p>
+    <div class="text">
+      <h3 class="title">お客様の商品交換日は○印の日です。</h3>
+      <p>◆レンタルモップ　◆レンタルマット　◆レンタル浄水器</p>
+      <p>★レンジフードフィルター　★エアコンクリーニング　★ハウスクリーニング</p>
+
+      <div class="text-left">
+        <p><img class="fit-picture" src="icon.png" alt="" height="200" width="200"></p>
+      </div>
+      <div class="text-right">
+        <h2>株 式 会 社 ク ラ イ ズ</h2>
+        <p>埼玉県北本市中丸7-153</p>
+        <p>TEL　０４８ー５０１ー７６５３</p>
+        <p>ﾌﾘｰﾀﾞｲｱﾙ　０１２０ー２１２１ー７４</p>
+      </div>
+    </div>
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
